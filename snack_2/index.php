e9bd094d670bce2beef21e3d58e5aafbc3ed06a7
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-15 12:53:59 +0200

    [Completed]

---
+++ snack_2/index.php
@@ -0,0 +1,47 @@
+<?php
+
+var_dump($_GET);
+$name = "$_GET[name]";
+$mail = "$_GET[mail]";
+$age = "$_GET[age]";
+
+if (strlen($name) > 3) {
+    if (str_contains($mail, '@')) {
+        if (is_numeric($age)) {
+            echo "Good job!";
+        } else {
+            echo "C'è qualcosa di sbagliato";
+        }
+        ;
+    } else {
+        echo "C'è qualcosa di sbagliato";
+    }
+    ;
+
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="" method="get">
+        Nome: <input type="text" name="name">
+        Mail: <input type="text" name="mail">
+        Età: <input type="number" name="age">
+        <button type="sumbit">SEND</button>
+    </form>
+
+</body>
+
+</html>
---
+++ snack_2/readme.md
@@ -0,0 +1,12 @@
+# Descrizione
+Con un form passare come parametri GET:
+- name, 
+- mail  
+- age  
+
+Verificare che:
+- name sia più lungo di 3 caratteri
+- mail contenga un punto e una chiocciola 
+- age sia un numero
+
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
